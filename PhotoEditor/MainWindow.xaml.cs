3213de50f6bd245b252ded51ac60862806dd745e
Omar taky <omartaky.ot@gmail.com>
2025-12-07 13:03:32 +0100

    first commit

---
+++ PhotoEditor/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace PhotoEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    
+
+        BitmapImage CurrentImage;
+        WriteableBitmap editableImage;
+
+        private void OpenImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Images|*.jpg;*.png;*.jpeg;*.bmp";
+
+            if (dlg.ShowDialog() == true)
+            {
+                CurrentImage = new BitmapImage(new Uri(dlg.FileName));
+                DisplayedImage.Source = CurrentImage;
+
+            }
+        }
+
+    }
+}
